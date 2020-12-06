0e5764ef856d862e31ffcb5e3c266f818efa01df
Philippe Yo <poub1@neuf.fr>
2020-12-06 14:32:20 +0100

    Add wheel management to display a simple animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgxWheelComponent, TextAlignment, TextOrientation } from 'ngx-wheel'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
+
+  @ViewChild(NgxWheelComponent) wheel;
+
   title = 'DailyWheel';
+  idToLandOn: any;
+  names: string[] = [ 'Mehdy', 'Philippe A.', 'Mathieu', 'Sebastien', 'Aurélien' ];
+  items: any[];
+  textOrientation: TextOrientation = TextOrientation.HORIZONTAL;
+  textAlignment: TextAlignment = TextAlignment.OUTER;
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.idToLandOn = this.names.indexOf(this.names[Math.floor(Math.random() * this.names.length)]);
+    const colors = ['green', 'yellow', 'orange', 'lightblue', 'pink'];
+    this.items = this.names.map((value) => ({
+      fillStyle: colors[this.names.indexOf(value) % colors.length],
+      text: `${value}`,
+      id: this.names.indexOf(value),
+      textFillStyle: 'black',
+      textFontSize: '16'
+    }));
+
+  }
+
+  reset() {
+    this.wheel.reset();
+  }
+
+  before() {
+    alert('Your wheel is about to spin');
+    this.playAudio();
+  }
+
+  async spin(prize) {
+    this.idToLandOn = prize;
+    await new Promise(resolve => setTimeout(resolve, 0));
+    this.wheel.spin();
+    this.playAudio();
+  }
+
+
+  after() {
+    alert('The winner is ' + this.names[this.idToLandOn]);
+  }
+
 }
